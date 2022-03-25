6138de0afcf428ed615ba96c1b41349d1fbd0562
n2ptune <n2ptune@outlook.com>
2022-03-25 16:07:32 +0900

    t

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql2')
 
-const conn = mysql.createConnection({
-	host: '172.30.179.19',
-	port: 3306,
-	user: 'root',
-	database: 'khlee',
-	password: 'tlansdlek2@'
-})
+// const conn = mysql.createConnection({
+// 	host: '172.30.179.19',
+// 	port: 3306,
+// 	user: 'root',
+// 	database: 'khlee',
+// 	password: 'tlansdlek2@'
+// })
 
 app.get('/', (req, res) => {
-	res.send({})
+	res.send('Hello World')
 })
 
 app.listen(5000, () => {
